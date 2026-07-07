b4abaadfc042f48e99301e9bcd33e65f8ad734d1
Python-Lettle <1071445082@qq.com>
2026-07-07 14:26:34 +0800

    🔧 Mod Config

--- src/minisnail/config.py
+++ src/minisnail/config.py
@@ -1,5 +1,5 @@
 import os
-import json
+import math
 from transformers import PretrainedConfig
 
 class SnailConfig(PretrainedConfig):
@@ -8,8 +8,8 @@ class SnailConfig(PretrainedConfig):
         '''Initialize a new configuration.
         '''
         # Extract PretrainedConfig-specific keys before they're consumed by kwargs.get
-        self.bos_token_id = kwargs.get("bos_token_id", 1)
-        self.eos_token_id = kwargs.get("eos_token_id", 2)
+        self.bos_token_id: int = kwargs.get("bos_token_id", 1)
+        self.eos_token_id: int = kwargs.get("eos_token_id", 2)
         super().__init__(bos_token_id=self.bos_token_id, eos_token_id=self.eos_token_id, **kwargs)
 
         # Path configuration
@@ -21,7 +21,7 @@ class SnailConfig(PretrainedConfig):
         self.epochs: int = kwargs.get("epochs", 2)
         self.batch_size: int = kwargs.get("batch_size", 32)
         self.learning_rate: float = kwargs.get("learning_rate", 0.001)
-        self.device: str = kwargs.get("device", "cuda:0")
+        self.device: str = torch.device(kwargs.get("device", "cuda:0"))
         self.dtype: str = kwargs.get("dtype", "bfloat16")
         self.num_workers: int = kwargs.get("num_workers", 8)
         self.accumulation_steps: int = kwargs.get("accumulation_steps", 8)
@@ -34,15 +34,18 @@ class SnailConfig(PretrainedConfig):
         self.vocab_size: int = kwargs.get("vocab_size", 6400)
 
         # Model configuration
-        self.d_model: int = kwargs.get("d_model", 768)                  # Model dimension
-        self.n_heads: int = kwargs.get("n_heads", 8)                    # Number of heads
-        self.d_ff: int = kwargs.get("d_ff", int(8/3*self.d_model))           # Feedforward dimension = 8/3 * d_model
-        self.max_seq_len: int = kwargs.get("max_seq_len", 340)          # Maximum sequence length of the model
-        self.hidden_act = kwargs.get("hidden_act", 'silu')
-        self.max_position_embeddings = kwargs.get("max_position_embeddings", 32768)
-        self.rms_norm_eps = kwargs.get("rms_norm_eps", 1e-6)
-        self.rope_theta = kwargs.get("rope_theta", 1e6)
-        
+        self.hidden_size: int = kwargs.get("hidden_size", 768)                      # Model dimension
+        self.num_hidden_layers: int = kwargs.get("num_hidden_layers", 8)            # Number of layers
+        self.num_attention_heads: int = kwargs.get("num_attention_heads", 8)        # Number of attention heads
+        self.intermediate_size: int = kwargs.get("intermediate_size", math.ceil(self.hidden_size * math.pi / 64) * 64)           # Feedforward dimension = 8/3 * hidden_size
+        self.max_seq_len: int = kwargs.get("max_seq_len", 340)                      # Maximum sequence length of the model
+        self.hidden_act: str = kwargs.get("hidden_act", 'silu')
+        self.max_position_embeddings: int = kwargs.get("max_position_embeddings", 32768)
+        self.rms_norm_eps: float = kwargs.get("rms_norm_eps", 1e-6)
+        self.rope_theta: float = kwargs.get("rope_theta", 1e6)
+        self.q_k_dim: int = kwargs.get("q_k_dim", 64)
+        self.attention_dropout: float = kwargs.get("attention_dropout", 0.0)
+
         # MOE configuration (Not used)
         self.use_moe: int = kwargs.get("use_moe", 0)                    # Whether to use MOE, 0 for no, 1 for yes
         
