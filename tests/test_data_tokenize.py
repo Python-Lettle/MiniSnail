ae6e1dee39014697ea6c9aa93c51bbf9c1434820
Python-Lettle <1071445082@qq.com>
2026-07-09 21:18:53 +0800

    ✨ Tokenizer Test

---
+++ tests/test_data_tokenize.py
@@ -0,0 +1,18 @@
+import argparse
+from transformers import AutoTokenizer
+
+from minisnail.util import read_memmap_data
+from minisnail.util import console
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tokenizer_path", type=str, default="./model/minimind")
+    args = parser.parse_args()
+
+    train_data = read_memmap_data("./data/train_dataset.npy")
+    console.print(train_data)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
+
+    raw_data = tokenizer.decode(train_data)
+    console.print(raw_data)
