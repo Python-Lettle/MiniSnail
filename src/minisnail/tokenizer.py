61b1c05bc64a7d5cccfbe632c9738da0253f10bb
Python-Lettle <1071445082@qq.com>
2026-07-09 21:16:45 +0800

    🔧 Mod Tokenizer

--- src/minisnail/tokenizer.py
+++ src/minisnail/tokenizer.py
@@ -1,6 +1,8 @@
+from minisnail.config import SnailConfig
 from transformers import AutoTokenizer
+from transformers.tokenization_utils_tokenizers import TokenizersBackend
 
-def get_tokenizer(config: SnailConfig):
+def get_tokenizer(config: SnailConfig) -> TokenizersBackend:
     '''Get a tokenizer using the options in config.
     '''
     return AutoTokenizer.from_pretrained(config.tokenizer_path)
